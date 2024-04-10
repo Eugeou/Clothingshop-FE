3afcaeaa55f3e8524713a1299855aa07c60665b7
Eugeou <21522495@gm.uit.edu.vn>
2024-04-11 01:56:48 +0700

    Design Home page banner

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -1,8 +1,42 @@
+"use-client";
+
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Slogan = () => {
+
+  const handleScroll = () => {
+
+  }
+
   return (
-    <div>Hero</div>
+    <div className="slogan">
+      <div className=" flex-1 pt-36 padding-x">
+        <h1 className="slogan__title">
+          Explore your true style!
+        </h1>
+
+        <p className="slogan__subtitle">
+          Easily and quickly to find and buy fashion clothes you need here!
+        </p>
+
+        <CustomButton 
+          title="Start shopping now"
+          containerStyles="bg-primary-blue
+          text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="slogan__image-container">
+        <div className="slogan__image">
+          <Image src="/fashion.png" alt="Banner" 
+          fill className=" object-contain" />
+
+          <div className="slogan__image-overlay"/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,8 @@
 'use client'
 import Slogan from "./Slogan"
+import CustomButton from "./CustomButton"
 
 export {
     Slogan,
+    CustomButton,
 }
